7de443569a9e115d519dfa3a4918fc80186bf5c2
domesya <163451587+domesya@users.noreply.github.com>
2026-05-10 21:15:45 +0200

    Update make-appimage.sh

--- make-appimage.sh
+++ make-appimage.sh
@@ -8,7 +8,7 @@ export ARCH VERSION
 export OUTPATH=./dist
 export ADD_HOOKS="self-updater.hook"
 export UPINFO="gh-releases-zsync|${GITHUB_REPOSITORY%/*}|${GITHUB_REPOSITORY#*/}|latest|*$ARCH.AppImage.zsync"
-export ICON=/usr/share/icons/Adwaita/scalable/actions/document-open-symbolic.svg
+export ICON=/usr/share/icons/hicolor/48x48/apps/system-file-manager.png
 export DESKTOP=./launcher.desktop
 
 # Deploy dependencies
