0a1b81fdb257511a137c417d0c86bbffb4b2d0b0
domesya <renski.k2@gmail.com>
2026-09-04 16:17:56 +0200

    more fixes 2

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -6,7 +6,7 @@ ARCH=$(uname -m)
 
 echo "Installing package dependencies..."
 echo "---------------------------------------------------------------"
-pacman -Syu --noconfirm libxkbcommon-x11 coreutils
+pacman -Syu --noconfirm libxkbcommon-x11
 
 echo "Installing debloated packages..."
 echo "---------------------------------------------------------------"
@@ -14,7 +14,7 @@ get-debloated-pkgs --add-common --prefer-nano ffmpeg-mini
 
 # Comment this out if you need an AUR package
 #make-aur-package zenity-rs-bin
-make-aur-package pikeru
+#make-aur-package pikeru
 
 # If the application needs to be manually built that has to be done down here
 
@@ -25,3 +25,17 @@ make-aur-package pikeru
 # else
 # 	regular build steps
 # fi
+
+wget https://raw.githubusercontent.com/dvhar/pikeru/refs/heads/master/PKGBUILD -O ./PKGBUILD
+pkgfile="./PKGBUILD"
+
+version=$(awk -F'=' '
+    $1 == "pkgver" { ver = $2 }
+    $1 == "pkgrel" { rel = $2 }
+    END { print ver "-" rel }
+' "$pkgfile")
+
+echo "$version" > ~/version
+
+make-aur-package ./PKGBUILD
+
