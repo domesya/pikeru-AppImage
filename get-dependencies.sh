565a25664830e6aee727de87c38cfad519f8545f
domesya <renski.k2@gmail.com>
2026-09-04 11:02:38 +0200

    improvements part 2

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -6,7 +6,7 @@ ARCH=$(uname -m)
 
 echo "Installing package dependencies..."
 echo "---------------------------------------------------------------"
-pacman -Syu --noconfirm libxkbcommon-x11
+pacman -Syu --noconfirm libxkbcommon-x11 coreutils
 
 echo "Installing debloated packages..."
 echo "---------------------------------------------------------------"
